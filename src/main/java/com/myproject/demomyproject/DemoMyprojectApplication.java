c88f27fc69bc7e0938f6dfcc215ecf1949173155
wudc <wudc@yahoo.com>
2019-04-08 11:52:58 -0400

    Added configuration and change to deploy the war file to Tomcat container and tested locally.

--- src/main/java/com/myproject/demomyproject/DemoMyprojectApplication.java
+++ src/main/java/com/myproject/demomyproject/DemoMyprojectApplication.java
@@ -2,13 +2,15 @@ package com.myproject.demomyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.myproject.demomyproject.repository.elasticsearch.KnowledgeElasticSearchRepository;
 import com.myproject.demomyproject.service.impl.SearchServiceImpl;
 
 @SpringBootApplication
-public class DemoMyprojectApplication { 
+public class DemoMyprojectApplication extends SpringBootServletInitializer { 
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DemoMyprojectApplication.class, args);
@@ -18,5 +20,10 @@ public class DemoMyprojectApplication {
 		System.out.println("DemoMyprojectApplication->KnowledgeElasticSearchRepository: " + repo);
 		
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoMyprojectApplication.class);
+    }
 
 }
